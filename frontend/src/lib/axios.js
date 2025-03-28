4026e39fe1f167a6eef74cb7c46a4bedbcb08ad4
Somethinn---Awkwarddd <somethinn999awkwardd@gmail.com>
2025-03-28 15:31:52 +0600

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-    baseURL: "https://store-backend-391l.onrender.com/api",
+    baseURL: " https://store-backend-391l.onrender.com/api",
     withCredentials: true, // send cookies to the server
 });
 
